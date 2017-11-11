60f05b95338a3cfdd059b3ea27ece3043d2a5a35
Rich Xu <richardxu100@gmail.com>
2017-11-11 16:41:11 -0500

    Add VueRouter and Firebase

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
     <app-navbar></app-navbar>  
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import { db } from './utils/firebase';
 
 export default {
   name: 'app',
   components: {
     'app-navbar': Navbar,
   },
-  // data() {
-    
-  // },
+  firebase: {
+    users: db.ref('users')
+  }
 }
 </script>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,39 @@
 import Vue from 'vue';
-import App from './App.vue';
+import VueFire from 'vuefire';
 import Buefy from 'buefy';
+import VueRouter from 'vue-router';
 
 import 'buefy/lib/buefy.css'
 
+import { routes } from './routes';
+import { db } from './utils/firebase';
+import App from './App.vue';
+
 Vue.use(Buefy);
+Vue.use(VueFire);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  render: h => h(App),
+  firebase: {
+    // simple syntax, bind as an array by default
+    anArray: db.ref('/'),
+    // can also bind to a query
+    // anArray: db.ref('url/to/my/collection').limitToLast(25)
+    // full syntax
+    anObject: {
+      source: db.ref('/'),
+      // optionally bind as an object
+      asObject: true,
+      // optionally provide the cancelCallback
+      cancelCallback: function () {}
+    }
+  }
 });
