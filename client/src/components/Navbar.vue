bf11bfae1b53f2f33a9753740593c0fd6a9f8062
Rich Xu <richardxu100@gmail.com>
2017-11-11 15:58:36 -0500

    Add navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <b>SHIFT</b>
+      </a>
+      <div class="navbar-burger burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link" href="/documentation/overview/start/">
+              Menu
+            </a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item" href="/documentation/overview/start/">
+                Leaderboard
+              </a>
+            <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
+                About
+              </a>
+          </div>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+            <button class="button is-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  // data() {
+    
+  // }
+}
+</script>
+
+<style lang="scss">
+
+</style>
